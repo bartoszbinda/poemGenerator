48ca51344d4c6b3f1e37a38bb512fe64e47e70fa
Bartosz Binda <batinex@gmail.com>
2016-05-18 07:06:20 +0200

    Added Data Grid

--- poemGenerator/poemGenerator/MainWindow.cs
+++ poemGenerator/poemGenerator/MainWindow.cs
@@ -1,17 +1,65 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.IO;
 using System.Windows;
 using System.Windows.Documents;
 using Microsoft.Win32;
+
 
 namespace PoemGenerator
 {
     /// <summary>
     ///     Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow
+    public static DataTable DataTableFromTextFile(string location, char delimiter = ',')
+{
+    DataTable result;
+
+    string[] LineArray = File.ReadAllLines(location);
+
+    result = FormDataTable(LineArray, delimiter);
+
+    return result;
+}
+
+private static DataTable FormDataTable(string[] LineArray, char delimiter)
+{
+    DataTable dt = new DataTable();
+
+    AddColumnToTable(LineArray, delimiter, ref dt);
+
+    AddRowToTable(LineArray, delimiter, ref dt);
+
+    return dt;
+}
+
+private static void AddRowToTable(string[] valueCollection, char delimiter, ref DataTable dt)
+{
+
+    for (int i = 1; i < valueCollection.Length; i++)
+    {
+        string[] values = valueCollection[i].Split(delimiter);
+        DataRow dr = dt.NewRow();
+        for (int j = 0; j < values.Length; j++)
+        {
+            dr[j] = values[j];
+        }
+        dt.Rows.Add(dr);
+    }
+}
+
+private static void AddColumnToTable(string[] columnCollection, char delimiter, ref DataTable dt)
+{
+    string[] columns = columnCollection[0].Split(delimiter);
+    foreach (string columnName in columns)
+    {
+        DataColumn dc = new DataColumn(columnName, typeof(string));
+        dt.Columns.Add(dc);
+    }
+}
+
+
+public partial class MainWindow
     {
         private readonly Random _random = new Random();
         private ArrayList _nouns, _verbs, _adverbs, _prepositions, _adjectives, _tempnouns;
@@ -34,7 +82,7 @@ namespace PoemGenerator
 
         private void SaveScript(string filename)
         {
-            if (string.IsNullOrEmpty(filename)) return;
+            if (String.isEmptyOrNull(filename)) return;
             var t = new TextRange(InputPoem.Document.ContentStart,
                 InputPoem.Document.ContentEnd);
             File.WriteAllText(filename, t.Text);
@@ -42,7 +90,7 @@ namespace PoemGenerator
 
         private void ReadScript(string filename)
         {
-            if (string.IsNullOrEmpty(filename)) return;
+            if (String.isEmptyOrNull(filename)) return;
             var scriptText = File.ReadAllLines(filename);
             foreach (var line in scriptText)
             {
@@ -77,6 +125,8 @@ namespace PoemGenerator
             Option.Visibility = Visibility.Visible;
             AppendToDictionary.Visibility = Visibility.Visible;
             editDictionary.Visibility = Visibility.Visible;
+
+
         }
 
         private void GeneratePoemItemClick1(object sender, RoutedEventArgs e)
@@ -88,7 +138,6 @@ namespace PoemGenerator
             Option.Visibility = Visibility.Hidden;
             AppendToDictionary.Visibility = Visibility.Hidden;
             editDictionary.Visibility = Visibility.Hidden;
-
         }
 
         private void appendToDictionary_Click(object sender, RoutedEventArgs e)
@@ -103,6 +152,8 @@ namespace PoemGenerator
                 {
                     if (line != null) File.AppendAllText("text\\noun.txt", line);
                 }
+                editDictionary.DataSource = Helper.DataTableFromTextFile("text\\noun.txt", "\n");
+
             }
             else if (Option.Text == "Verb")
             {
@@ -114,6 +165,8 @@ namespace PoemGenerator
                 {
                     if (line != null) File.AppendAllText("text\\verb.txt", line);
                 }
+                editDictionary.DataSource = Helper.DataTableFromTextFile("text\\verb.txt", "\n");
+
             }
 
             else if (Option.Text == "Adverb")
@@ -126,6 +179,8 @@ namespace PoemGenerator
                 {
                     if (line != null) File.AppendAllText("text\\adverb.txt", line);
                 }
+                editDictionary.DataSource = Helper.DataTableFromTextFile("text\\adverb.txt", "\n");
+
             }
 
             else if (Option.Text == "Preposition")
@@ -138,6 +193,8 @@ namespace PoemGenerator
                 {
                     if (line != null) File.AppendAllText("text\\preposition.txt", line);
                 }
+                editDictionary.DataSource = Helper.DataTableFromTextFile("text\\preposition.txt", "\n");
+               
             }
 
             else if (Option.Text == "Tempnoun")
@@ -150,6 +207,8 @@ namespace PoemGenerator
                 {
                     if (line != null) File.AppendAllText("text\\tempnoun.txt", line);
                 }
+                editDictionary.DataSource = Helper.DataTableFromTextFile("text\\tempnoun.txt", "\n");
+
             }
 
             else if (Option.Text == "Adjective")
@@ -162,7 +221,10 @@ namespace PoemGenerator
                 {
                     if (line != null) File.AppendAllText("text\\adjective.txt", line);
                 }
+                editDictionary.DataSource = Helper.DataTableFromTextFile("text\\adjective.txt", "\n");
+
             }
+
         }
 
         private void NewScriptMenuItemClick2(object sender, RoutedEventArgs e)
@@ -177,11 +239,9 @@ namespace PoemGenerator
 
         private void OpenScriptMenuItemClick5(object sender, RoutedEventArgs e)
         {
-            var newScript = new OpenFileDialog
-            {
-                DefaultExt = ".txt",
-                Filter = "Text document  (*.txt)|*.txt"
-            };
+            var newScript = new OpenFileDialog();
+            newScript.DefaultExt = ".txt";
+            newScript.Filter = "Text document  (*.txt)|*.txt";
             newScript.ShowDialog();
             var filename = newScript.FileName;
             ReadScript(filename);
@@ -202,41 +262,41 @@ namespace PoemGenerator
                         switch (line[i])
                         {
                             case 'N':
-                                {
-                                    var r = _random.Next(_nouns.Count);
+                            {
+                                var r = _random.Next(_nouns.Count);
 
-                                    OutputPoem.AppendText((string)_nouns[r] + " ");
-                                }
+                                OutputPoem.AppendText((string) _nouns[r] + " ");
+                            }
                                 break;
                             case 'A':
-                                {
-                                    var r = _random.Next(_adjectives.Count);
-                                    OutputPoem.AppendText((string)_adjectives[r] + " ");
-                                }
+                            {
+                                var r = _random.Next(_adjectives.Count);
+                                OutputPoem.AppendText((string) _adjectives[r] + " ");
+                            }
                                 break;
                             case 'V':
-                                {
-                                    var r = _random.Next(_verbs.Count);
-                                    OutputPoem.AppendText((string)_verbs[r] + " ");
-                                }
+                            {
+                                var r = _random.Next(_verbs.Count);
+                                OutputPoem.AppendText((string) _verbs[r] + " ");
+                            }
                                 break;
                             case 'D':
-                                {
-                                    var r = _random.Next(_adverbs.Count);
-                                    OutputPoem.AppendText((string)_adverbs[r] + " ");
-                                }
+                            {
+                                var r = _random.Next(_adverbs.Count);
+                                OutputPoem.AppendText((string) _adverbs[r] + " ");
+                            }
                                 break;
                             case 'P':
-                                {
-                                    var r = _random.Next(_prepositions.Count);
-                                    OutputPoem.AppendText((string)_prepositions[r] + " ");
-                                }
+                            {
+                                var r = _random.Next(_prepositions.Count);
+                                OutputPoem.AppendText((string) _prepositions[r] + " ");
+                            }
                                 break;
                             case 'T':
-                                {
-                                    var r = _random.Next(_tempnouns.Count);
-                                    OutputPoem.AppendText((string)_tempnouns[r] + " ");
-                                }
+                            {
+                                var r = _random.Next(_tempnouns.Count);
+                                OutputPoem.AppendText((string) _tempnouns[r] + " ");
+                            }
                                 break;
                             default:
                                 if (line[i - 1] == '#' &&
